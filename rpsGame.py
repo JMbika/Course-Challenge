4cec0d525cd3fa7e9902d2a0333ab1a75a9eae1d
Joyce Mbika <mjoyce@live.ca>
2019-04-25 13:55:41 -0400

    In order to give the play an option if they wanted to play again, the game while loop was put in a function, so it can be called in the play again loop incase the user wanted to play again

--- rpsGame.py
+++ rpsGame.py
@@ -1,82 +1,96 @@
 import random
 
-#Score Count
-userScore = 0
-computerScore = 0
+def rps_game():
+    
+    #Score Count
+    userScore = 0
+    computerScore = 0
 
-#Lives Count
-userLife = 3
-computerLife = 3
-choices = ["r", "p", "s"]
-rnd = 1
+    #Lives Count
+    userLife = 3
+    computerLife = 3
+    
+    choices = ["r", "p", "s"]
+    
+    rnd = 1
 
+    while True:
+        print("Round: ", rnd)
+        userChoice = input("What do you pick (r)ock, (p)aper, (s)cissors?")
+        computerChoice = random.choice(choices)
+        print("Computer chose:", computerChoice)
 
-while True:
-    print("Round: ", rnd)
-    userChoice = input("What do you pick (r)ock, (p)aper, (s)cissors?")
-    computerChoice = random.choice(choices)
-    print("Computer chose:", computerChoice)
+        if len(userChoice) == 0:
+            print("Invalid answer. Try again")
+            continue
 
-    if len(userChoice) == 0:
-        print("Invalid answer. Try again")
+        if userChoice[0].lower() == computerChoice:
+            print("Tie!")
+            continue
+                    
+        elif userChoice[0].lower() == "r" and  computerChoice == "s":
+            print("Computer Loses")
+            userScore += 1
+            computerLife -= 1
+            if computerLife == 0:
+                break
 
-    if userChoice[0].lower() == computerChoice:
-        print("Tie!")
-                
-    elif userChoice[0].lower() == "r" and  computerChoice == "s":
-        print("Computer Loses")
-        userScore += 1
-        computerLife -= 1
-        if computerLife == 0:
+        elif userChoice[0].lower() == "s" and  computerChoice == "r":
+            print("User Loses")
+            computerScore += 1
+            userLife -= 1
+            if userLife == 0:
+                break
+
+        elif userChoice[0].lower() == "p" and  computerChoice == "s":
+            print("User Loses")
+            computerScore += 1
+            userLife -= 1
+            if userLife == 0:
+                break
+
+        elif userChoice[0].lower() == "s" and  computerChoice == "p":
+            print("Computer Loses")
+            userScore += 1
+            computerLife -= 1
+            if computerLife == 0:
+                break
+
+        elif userChoice[0].lower() == "r" and  computerChoice == "p":
+            print("User Loses")
+            computerScore += 1
+            userLife -= 1
+            if userLife == 0:
+                break
+                    
+        elif userChoice[0].lower() == "p" and  computerChoice == "r":
+            print("Computer Loses")
+            userScore += 1
+            computerLife -= 1
+            if computerLife == 0:
+                break
+
+        else:
+            print("Invalid Answer. Try again!")
+
+        rnd = rnd + 1
+        if rnd == 4:
             break
 
-    elif userChoice[0].lower() == "s" and  computerChoice == "r":
-        print("User Loses")
-        computerScore += 1
-        userLife -= 1
-        if userLife == 0:
-            break
-
-    elif userChoice[0].lower() == "p" and  computerChoice == "s":
-        print("User Loses")
-        computerScore += 1
-        userLife -= 1
-        if userLife == 0:
-            break
-
-    elif userChoice[0].lower() == "s" and  computerChoice == "p":
-        print("Computer Loses")
-        userScore += 1
-        computerLife -= 1
-        if computerLife == 0:
-            break
-
-    elif userChoice[0].lower() == "r" and  computerChoice == "p":
-        print("User Loses")
-        computerScore += 1
-        userLife -= 1
-        if userLife == 0:
-            break
-                
-    elif userChoice[0].lower() == "p" and  computerChoice == "r":
-        print("Computer Loses")
-        userScore += 1
-        computerLife -= 1
-        if computerLife == 0:
-            break
-
+    print("User Wins: ", userScore)
+    print("Computer wins: ", computerScore)
+    if userScore > computerScore:
+        print("The User wins the game!")
+    elif computerScore > userScore:
+        print("The Computer wins the game!")
     else:
-        print("Invalid Answer. Try again!")
+        print("It's a tie!")
+rps_game()
 
-    rnd = rnd + 1
-    if rnd == 4:
+answer = True
+while answer:
+    answer = input("Play again (y)es, (n)o?")
+    if answer == "y":
+        rps_game()
+    else:
         break
-
-print("User Wins: ", userScore)
-print("Computer wins: ", computerScore)
-if userScore > computerScore:
-    print("The User wins the game!")
-elif computerScore > userScore:
-    print("The Computer wins the game!")
-else:
-    print("It's a tie!")
