ff5e6626804f8b675b8afb34ba00d417d17be46e
Joyce Mbika <mjoyce@live.ca>
2019-04-25 13:07:42 -0400

    Comparing the user and computer choices

--- rpsGame.py
+++ rpsGame.py
@@ -1,8 +1,44 @@
 import random
 
+#Score Count
+userScore = 0
+computerScore = 0
+
+#Lives Count
+userLife = 3
+computerLife = 3
+
 choices = ["r", "p", "s"]
+
+userChoice = input("What do you pick (r)ock, (p)aper, (s)cissors?")
 
 computerChoice = random.choice(choices)
 print("Computer chose:", computerChoice)
 
-userChoice = input("What do you pick (r)ock, (p)aper, (s)cissors?")
+#compare the choices
+if len(userChoice) == 0:
+            print("Invalid answer. Try again")
+
+if userChoice[0].lower() == computerChoice:
+    print("Tie!")
+            
+elif userChoice[0].lower() == "r" and  computerChoice == "s":
+    print("Computer Loses")
+
+elif userChoice[0].lower() == "s" and  computerChoice == "r":
+    print("User Loses")
+
+elif userChoice[0].lower() == "p" and  computerChoice == "s":
+    print("User Loses")
+
+elif userChoice[0].lower() == "s" and  computerChoice == "p":
+    print("Computer Loses")
+
+elif userChoice[0].lower() == "r" and  computerChoice == "p":
+    print("User Loses")
+            
+elif userChoice[0].lower() == "p" and  computerChoice == "r":
+    print("Computer Loses")
+
+else:
+    print("Invalid Answer. Try again!")
